e33a139bb7e32e9f8129b444cfb120394c6c9270
Dave Jacobowitz <david.jacobowitz@gmail.com>
2018-12-14 18:32:55 -0800

    read from buttons

---
+++ rpi/drivers/buttons.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+real_gpio = False
+try:
+    import RPi.GPIO as GPIO
+except Exception as e:
+    from drivers import dummy_gpio as GPIO
+
+class Buttons(object):
+    def __init__(self):
+        self.bpins = [ 7, 12 ]
+        GPIO.setmode(GPIO.BOARD)
+        for bpin in self.bpins:
+            GPIO.setup(bpin, GPIO.IN)
+    def pressed(self):
+        return [ False if GPIO.input(pin) else True for pin in self.bpins ]
+    
+
+if __name__ == '__main__':
+    import time
+    buttons = Buttons()
+
+    while True:
+        v = buttons.pressed()
+        if any(v):
+            print(v)
+        time.sleep(1)
