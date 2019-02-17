4f5b4b261eb63a54d4b06a7f46f7da5284295b95
Dave Jacobowitz <david.jacobowitz@gmail.com>
2019-02-17 10:27:16 -0800

    messages for low-level debugging without real hardware

--- rpi/drivers/dummy_gpio.py
+++ rpi/drivers/dummy_gpio.py
@@ -1,17 +1,21 @@
 
-OUT = 0
-IN  = 1
-LOW = 2
-HIGH = 3
-BOARD = 4
-PUD_DOWN = 5
+OUT      = 'out' 
+IN       = 'in' 
+LOW      = 'low' 
+HIGH     = 'high' 
+BOARD    = 'board'
+PUD_DOWN = 'pud_down'
+PUD_UP   = 'pud_up'
 
-def setmode(x):
-    pass
+def setmode(pin_order):
+    return
+    print('DummyGPIO setupmode order={}'.format(pin_order))
 
-def setup(x,y,pull_up_down = None):
-    pass
+def setup(pin,mode,pull_up_down = None):
+    return
+    print('DummyGPIO setup pin={}, direction={}, mode={}'.format(pin,mode,pull_up_down))
 
-def output(x,y):
-    pass
+def output(pin,value):
+    return
+    print('DummyGPIO output pin={}, value={}'.format(pin,value))
 
