e818151cb0dfc88d3a98b23653c26b1c2799af35
Dave Jacobowitz <david.jacobowitz@gmail.com>
2019-02-17 10:28:21 -0800

    smarten metar to use last 24 hours for precip totals

--- rpi/metar.py
+++ rpi/metar.py
@@ -21,21 +21,71 @@ def jss(x):
     return json.dumps(x,indent=2,sort_keys=True)
 
 class Metar:
-    def __init__(self,data):
-        self.data = data 
+
+    def __init__(self):
+        pass
+    def fetch(self, cfg):
+        data = _fetch(cfg)
+        if data and data.get('response',None) and data['response'].get('data',None) and data['response']['data'].get('METAR',None):
+
+            metar = data['response']['data']['METAR']
+            if isinstance(metar,list):
+                # get data as newest first
+                self.data = sorted(metar, key=lambda x: x['observation_time'], reverse=True)
+                # print(jss(self.data))
+            else:
+                self.data = [ metar ]
+
+            self.latest = self.data[0]
+            return self.data
+        else:
+            return None
+
     def text(self):
-        return self.data['raw_text']
-    def _get_var(self,vn,number = True):
-        t = self.data.get(vn,None)
-        if t and number:
-            t = float(t)
-        return t
+        return self.latest['raw_text']
+
+    def _get_var(self,vn,mode='last_number'):
+        metars_to_sum = [ self.latest ]
+
+        if (mode == 'sum' or mode =='average'):
+            metars_to_sum = self.data
+        
+        rv = None 
+
+        for metar in metars_to_sum:
+            t = metar.get(vn,None)
+            if t:
+                if mode == 'last_text':
+                    rv = t
+                elif mode == 'min':
+                    t = float(t)
+                    if rv is None or t < rv:
+                        rv = t
+                elif mode == 'max':
+                    t = float(t)
+                    if rv is None or t > rv:
+                        rv = t
+                else:
+                    if rv is None:
+                        rv = 0
+                    rv += float(t)
+            else:
+                pass
+
+        if mode == 'average':
+            rv /= len(metars_to_sum)
+
+        return rv
+
     def temp(self):
         return self._get_var('temp_c')
-    def precip_inches(self):
-        return self._get_var('precip_in')
+    def precip_inches(self,sum24=False):
+        rv = self._get_var('precip_in', 'sum' if sum24 else 'last_number')
+        if rv is None:
+            return 0
+        return rv
     def wx(self):
-        return self._get_var('wx_string',False)
+        return self._get_var('wx_string','last_text')
     def freezing(self):
         t = self.temp()
         if t is not None:
@@ -57,12 +107,12 @@ class Metar:
     def getAll(self):
         return self.data if self.data else None
 
-def get(cfg):
-    data = _fetch(cfg)
-    if data and data.get('response',None) and data['response'].get('data',None) and data['response']['data'].get('METAR',None):
-
-        metar = data['response']['data']['METAR']
-        return Metar(metar)
+#def get(cfg):
+#    data = _fetch(cfg)
+#    if data and data.get('response',None) and data['response'].get('data',None) and data['response']['data'].get('METAR',None):
+#
+#        metar = data['response']['data']['METAR']
+#        return Metar(metar)
 
 
 if __name__ == '__main__':
@@ -72,10 +122,18 @@ if __name__ == '__main__':
             'dataSource': 'metars',
             'requestType': 'retrieve',
             'format': 'xml',
-            'hoursBeforeNow':1,
+            'hoursBeforeNow':24,
             'mostRecent':'true',
-            'stationString':'KMBS',
+            'stationString':'KOAK',
         }
     }
 
-    print(get(wcfg))
+    m = Metar()
+    m.fetch(wcfg)
+    print('wx str: {}'.format(m.text()))
+    print('temp  : {}'.format(m.temp()))
+    print('wx    : {}'.format(m.wx()))
+    print('freez : {}'.format(m.freezing()))
+    print('precip: {}'.format(m.precip_inches()))
+    print('precip24: {}'.format(m.precip_inches(True)))
+
